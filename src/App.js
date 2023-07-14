1f65c2d96d4d72c0f66b04892564287a47e58df1
Liam Gosnell <liam@wolfgangdigital.com>
2023-07-14 11:37:50 +0100

    full build with packages test

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from 'react-router-dom';
 
 import Topbar from './scenes/global/TopBar';
-// import Login from './scenes/login'
+
 import Home from './scenes/home';
 import Sidebar from './scenes/global/Sidebar';
-// import Logtime from './scenes/Logtime';
-// import Reports from './scenes/reports';
-// import Admin from './scenes/admin';
-// import Calendar from './scenes/calendar'
+
 
 
 
@@ -26,12 +23,9 @@ function App() {
               <main className='content'>
                 <Topbar/>
                 <Routes>
-                    {/* <Route path="/login" element={<login/>} /> */}
-                    {/* <Route path="/login" element={<Login/>} /> */}
-                    {/* <Route path="/home" element={<logtime/>} /> */}
+
                     <Route path="/" element={<Home/>} />
-                    {/* <Route path="/Admin" element={<admin/>} /> */}
-                    {/* <Route path="/Calendar" element={<calendar/>} /> */}
+
                 </Routes>
               </main>
             </div>
